fffbdea829e17a3bec3a6e8c2e7ff97000790d20
Glitch (glitch-hello-website) <none>
2022-04-15 18:12:59 +0000

    🍥🌇 Checkpoint ./tamamlandi.j:6542236/323

---
+++ tamamlandi.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+        <meta charset="utf-8">        
+        <title>Yönlendirme</title>
+        <meta http-equiv="refresh" content="6;URL=https://www.hitapakasabasi.tk/">
+ 
+    </head>
+ 
+    <body>    
+    <h1>Böyle sayfa bulunamadı 5 saniye sonra ana  sayfaya yönlendirileceksiniz</h1>
+    </body>
+</html>
